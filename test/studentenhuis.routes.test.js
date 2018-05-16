4d900e3410ee502a12c24c184f8807a2ab32ce1b
Jorrit Meeuwissen <jorritjeah@gmail.com>
2018-05-16 14:41:32 +0200

    studentenhuis routes test updated

--- test/studentenhuis.routes.test.js
+++ test/studentenhuis.routes.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjcyMzc4NzEsImlhdCI6MTUyNjM3Mzg3MSwic3ViIjp7InVzZXIiOnsiaWQiOjMsImZpcnN0bmFtZSI6IkpvcnJpdCIsImxhc3RuYW1lIjoiTWVldXdpc3NlbiIsImVtYWlsIjoiam9ycml0LW1lZXV3aXNzZW5AaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRDSnRQNlNQMGh0dTdRTURPZW51RU1lTmtuQXdkWjZudGdINVc1YjZTSGRXRXVpcTk3SG9MYSJ9fX0._V3G_WVk6-v1pN9CiYfz7e1Afnz5nZO2-2X6xCde5jA";
+let token = "";
 const invalidToken = "eyJ0eXAAAAAAAajIUzI1NiJ9.eyJleHAiOjE1MjcyMzc4NzEsImlhdCI6MTUyNjM3Mzg3MSwic3ViIjp7InVzZXIiOnsiaWQiOjMsImZpcnN0bmFtZSI6IkpvcnJpdCIsImxhc3RuYW1lIjoiTWVldXdpc3NlbiIsImVtYWlsIjoiam9ycml0LW1lZXV3aXNzZW5AaG90bWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRDSnRQNlNQMGh0dTdRTURPZW51RU1lTmtuQXdkWjZudGdINVc1YjZTSGRXRXVpcTk3SG9MYSJ9fX0._V3G_WVk6-v1pN9CiYfz7e1Afnz5nZO2-2X6xCde5jA";
 let studentenhuisID = 0;
 const naam = "TestNaam";
@@ -11,6 +11,22 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('Studentenhuis API POST', () => {
+    before(function(){
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "test@test.nl",
+                "password": "secret"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+
+                token = res.body.token;
+            });
+
+
+    });
     it('should throw an error when using invalid JWT token', (done) => {
         chai.request(server)
             .post('/api/studentenhuis')
@@ -191,57 +207,123 @@ describe('Studentenhuis API GET one', () => {
 
 describe('Studentenhuis API PUT', () => {
     it('should throw an error when using invalid JWT token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
+        chai.request(server)
+            .put('/api/studentenhuis/' + studentenhuisID)
+            .set(
+                "x-access-token", invalidToken
+            )
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+                response.should.have.property('message');
+                response.should.have.property('code');
+                response.should.have.property('datetime');
+                done();
+            });
+    });
 
     it('should return a studentenhuis with ID when posting a valid object', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
+        chai.request(server)
+            .put('/api/studentenhuis/' + studentenhuisID)
+            .send({
+                "naam": "nieuwenaam",
+                "adres": "Nieuw Adres"
+            }).set(
+            "x-access-token", token
+        )
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+                response.should.have.property('ID');
+                studentenhuisID = response.ID;
+                response.should.have.property('naam').equals("nieuwenaam");
+                response.should.have.property('adres').equals("Nieuw Adres");
+                response.should.have.property('contact');
+                response.should.have.property('email');
+                done();
+            });
+    });
 
     it('should throw an error when naam is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
+        chai.request(server)
+            .put('/api/studentenhuis/' + studentenhuisID)
+            .send({
+                "adres": "Nieuw Adres"
+            }).set(
+            "x-access-token", token
+        )
+            .end((err, res) => {
+                res.should.have.status(412);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+                response.should.have.property('message');
+                response.should.have.property('code');
+                response.should.have.property('datetime');
+                done();
+            });
+    });
 
     it('should throw an error when adres is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+            .put('/api/studentenhuis/' + studentenhuisID)
+            .send({
+                "naam": "nieuwenaam"
+            }).set(
+            "x-access-token", token
+        )
+            .end((err, res) => {
+                res.should.have.status(412);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+                response.should.have.property('message');
+                response.should.have.property('code');
+                response.should.have.property('datetime');
+                done();
+            });
     })
-})
+});
 
 describe('Studentenhuis API DELETE', () => {
     it('should throw an error when using invalid JWT token', (done) => {
+        chai.request(server)
+            .delete('/api/studentenhuis/' + studentenhuisID)
+            .set(
+                "x-access-token", invalidToken
+            )
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+                response.should.have.property('message');
+                response.should.have.property('code');
+                response.should.have.property('datetime');
+                done();
+            });
+    });
+
+    it('should return a succesfull delete message when deleting a studentenhuis', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
         done()
     })
 
-    it('should return a studentenhuis when posting a valid object', (done) => {
+    it('should throw an error when there are meals in a studentenhuis', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
         done()
     })
 
-    it('should throw an error when naam is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
 
-    it('should throw an error when adres is missing', (done) => {
+    it('should throw an error when the user is not the creator of studentenhuis', (done) => {
         //
         // Hier schrijf je jouw testcase.
         //
